c44b1ed533fbd003ed72893e4eb05054969db0d6
Nico-S91 <niko.sibi@gmail.com>
2018-04-01 12:40:53 -0300

    Colores + facebook
    Se cambiaron los colores de la app, se esta implementando login contra facebook.

--- app/src/main/java/tp1/g3/tdp2/hoycomo/MainActivity.java
+++ app/src/main/java/tp1/g3/tdp2/hoycomo/MainActivity.java
@@ -1,7 +1,14 @@
 package tp1.g3.tdp2.hoycomo;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        printKeyHash();
     }
+
+    private void printKeyHash(){
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo("tp1.g3.tdp2.hoycomo", PackageManager.GET_SIGNATURES);
+
+            for (Signature signature:info.signatures){
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d(tag: "KeyHash", Base64.encodeToString(md.digest(), Base64.DEFAULT))
+            }
+
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
