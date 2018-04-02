8b40b48dca475ad0920a50c698c71e1551e70947
Nico-S91 <niko.sibi@gmail.com>
2018-04-02 18:27:42 -0300

    Facebook Login Completo
    Se termino el login contra facebook

--- app/src/main/java/tp1/g3/tdp2/hoycomo/MainActivity.java
+++ app/src/main/java/tp1/g3/tdp2/hoycomo/MainActivity.java
@@ -1,23 +1,125 @@
 package tp1.g3.tdp2.hoycomo;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.Signature;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Base64;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+
+    CallbackManager callbackManager;
+    TextView txtEmail, txtBithday, txtFriends;
+    ProgressDialog mDialog;
+    ImageView imgAvatar;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        printKeyHash();
+        callbackManager = CallbackManager.Factory.create();
+
+        txtBithday = (TextView) findViewById(R.id.txtBirthday);
+        txtEmail = (TextView) findViewById(R.id.txtEmail);
+        txtFriends = (TextView) findViewById(R.id.txtFriends);
+
+        imgAvatar = (ImageView) findViewById(R.id.avatar);
+
+        final LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setReadPermissions(Arrays.asList("public_profile", "email", "user_birthday", "user_friends"));
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                mDialog = new ProgressDialog(MainActivity.this);
+                mDialog.setMessage("Retrieving data...");
+                mDialog.show();
+
+                String accessToken = loginResult.getAccessToken().getToken();
+
+                GraphRequest request = GraphRequest.newMeRequest(loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
+                    @Override
+                    public void onCompleted(JSONObject object, GraphResponse response) {
+                        mDialog.dismiss();
+                        Log.d("response", response.toString());
+                        getData(object);
+                    }
+                });
+
+                //API de Graph
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "ïd,email,birthday,friends");
+                request.setParameters(parameters);
+                request.executeAsync();
+
+                //TODO: Testear en un celular real!
+
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
+
+        if(AccessToken.getCurrentAccessToken() != null){
+            txtEmail.setText(AccessToken.getCurrentAccessToken().getUserId());
+        }
+
+    }
+
+    private void getData(JSONObject object) {
+        try {
+            URL profile_picture = new URL("https://graph.facebook.com/" + object.getString("id")+"/picture?width=250&height=250");
+            Picasso.with(this).load(profile_picture.toString()).into(imgAvatar);
+
+            txtEmail.setText(object.getString("email"));
+            txtBithday.setText(object.getString("birthday"));
+            txtFriends.setText("Friends: " + object.getJSONObject("friends").getJSONObject("summary").getString("total_count"));
+
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 
     private void printKeyHash(){
@@ -27,7 +129,6 @@ public class MainActivity extends AppCompatActivity {
             for (Signature signature:info.signatures){
                 MessageDigest md = MessageDigest.getInstance("SHA");
                 md.update(signature.toByteArray());
-                Log.d(tag: "KeyHash", Base64.encodeToString(md.digest(), Base64.DEFAULT))
             }
 
         } catch (PackageManager.NameNotFoundException e) {
